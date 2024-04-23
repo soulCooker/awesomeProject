c7f8da25eeae46beb2ebac79ff347b0730c92783
liaoxingjie <liaoxingjie@58.com>
2024-04-23 15:02:12 +0800

    tree traverse

--- tmp/tree/in_order_traverse.go
+++ tmp/tree/in_order_traverse.go
@@ -27,17 +27,60 @@ package tree
 	节点为已访问，节点出栈并处理
 */
 
+/*
+中序
+访问栈顶节点
+
+	节点为已访问，节点出栈并处理，右节点进栈
+*/
+func inorderTraversal(root *TreeNode) []int {
+	stack := make([]*VisitedNode, 0)
+	res := make([]int, 0)
+
+	if root == nil {
+		return nil
+	}
+
+	stack = append(stack, &VisitedNode{
+		Node:    root,
+		Visited: 0,
+	})
+
+	for len(stack) > 0 {
+		cur := stack[len(stack)-1]
+		//节点为未访问，标记节点为已访问，左节点进栈
+		if cur.Visited == 0 {
+			cur.Visited = 1
+			if cur.Node.Left != nil {
+				stack = append(stack, &VisitedNode{
+					Node:    cur.Node.Left,
+					Visited: 0,
+				})
+			}
+		} else { //节点为已访问，节点出栈并处理，右节点进栈
+			res = append(res, cur.Node.Val)
+			if cur.Node.Right != nil {
+				stack[len(stack)-1].Visited = 0
+				stack[len(stack)-1].Node = cur.Node.Right
+			} else {
+				stack = stack[:len(stack)-1]
+			}
+		}
+	}
+	return res
+}
+
 type VisitedNode struct {
-	Node   *TreeNode
-	Depth  int
-	Status int
+	Node    *TreeNode
+	Depth   int
+	Visited int
 }
 
 func NewVNode(node *TreeNode, depth int) *VisitedNode {
 	return &VisitedNode{
-		Node:   node,
-		Status: 0,
-		Depth:  depth,
+		Node:    node,
+		Visited: 0,
+		Depth:   depth,
 	}
 }
 
@@ -55,11 +98,11 @@ func doInorderTraversal(root *TreeNode, visited func(node *VisitedNode) interfac
 	for len(stack) > 0 {
 		vNode := stack[len(stack)-1]
 		tNode := vNode.Node
-		if vNode.Status == 0 {
+		if vNode.Visited == 0 {
 			if tNode.Left != nil {
 				stack = append(stack, NewVNode(tNode.Left, vNode.Depth+1))
 			}
-			vNode.Status = 1
+			vNode.Visited = 1
 		} else {
 			res = visited(vNode)
 			if tNode.Right != nil {
@@ -95,12 +138,12 @@ func PreorderTraversal(root *TreeNode) []int {
 	for len(stack) > 0 {
 		vNode := stack[len(stack)-1]
 		tNode := vNode.Node
-		if vNode.Status == 0 {
+		if vNode.Visited == 0 {
 			if tNode.Left != nil {
 				stack = append(stack, NewVNode(tNode.Left, 0))
 			}
 			res = append(res, tNode.Val)
-			vNode.Status = 1
+			vNode.Visited = 1
 		} else {
 			if tNode.Right != nil {
 				stack[len(stack)-1] = NewVNode(tNode.Right, 0)
--- tmp/tree/max_depth.go
+++ tmp/tree/max_depth.go
@@ -7,15 +7,41 @@ package tree
  *     Left *TreeNode
  *     Right *TreeNode
  * }
- */
+深度优先遍历，先序遍历
+节点出栈并更新最大深度=max(当前最大深度,节点深度)，左节点、右节点进栈。子节点深度为节点深度+1
+*/
 
 func MaxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
 	maxDepth := 0
-	doInorderTraversal(root, func(node *VisitedNode) interface{} {
-		if node.Depth > maxDepth {
-			maxDepth = node.Depth
-		}
-		return maxDepth
+
+	stack := make([]*DepthNode, 0)
+	stack = append(stack, &DepthNode{
+		TreeNode: root,
+		Depth:    1,
 	})
+
+	for len(stack) > 0 {
+		curRoot := stack[len(stack)-1]
+		if curRoot.Depth > maxDepth {
+			maxDepth = curRoot.Depth
+		}
+		stack = stack[:len(stack)-1]
+		if curRoot.Left != nil {
+			stack = append(stack, &DepthNode{
+				Depth:    curRoot.Depth + 1,
+				TreeNode: curRoot.Left,
+			})
+		}
+		if curRoot.Right != nil {
+			stack = append(stack, &DepthNode{
+				Depth:    curRoot.Depth + 1,
+				TreeNode: curRoot.Right,
+			})
+		}
+	}
+
 	return maxDepth
 }
